f230ed0e178472e6e74c2f53a9648a25714105ee
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2022-11-01 16:50:56 -0300

    Add auth store

---
+++ src/stores/auth.ts
@@ -0,0 +1,71 @@
+import { defineStore } from "pinia";
+import { useLocalStorage } from "@vueuse/core";
+import { useJwt } from "@vueuse/integrations/useJwt";
+
+export const useAuth = defineStore("auth", {
+  state: () => ({
+    user: {
+      token: useLocalStorage("olms-token", ""),
+    },
+  }),
+
+  getters: {
+    userAuthenticated: (state) => state.user.token.length > 0,
+    userData: (state) => {
+      const { payload } = useJwt(state.user.token);
+      return payload.value;
+    },
+  },
+
+  actions: {
+    async login(email: string, password: string) {
+      const res = await fetch(import.meta.env.VITE_API_URL + "/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+
+      const json = await res.json();
+
+      if (res.ok) {
+        this.user.token = json.access_token ?? "";
+      } else {
+        throw new Error(json.detail);
+      }
+    },
+
+    logout() {
+      this.user.token = "";
+    },
+
+    async protectedFetch(
+      path: string,
+      method: "GET" | "POST" = "POST",
+      payload: object = {}
+    ) {
+      if (this.user.token.length < 1) return;
+
+      const res = await fetch(import.meta.env.VITE_API_URL + path, {
+        method: method,
+        headers: {
+          Authorization: "Bearer " + this.user.token,
+          "Content-Type": "application/json",
+        },
+        body: method === "POST" ? JSON.stringify(payload) : null,
+      });
+
+      const json = await res.json();
+
+      if (res.ok) {
+        return json;
+      } else {
+        return new Error(json.detail);
+      }
+    },
+  },
+});
