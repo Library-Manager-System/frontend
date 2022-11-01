0a8929d4c25a64a68e1fe1eb78d6af2d54730557
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2022-11-01 16:56:44 -0300

    Add redirect component

---
+++ src/components/RedirectTo.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+export default {
+  props: {
+    name: {
+      required: true,
+      type: String,
+    },
+  },
+
+  mounted() {
+    this.$router.replace({ name: this.name });
+  },
+};
+</script>
+
+<template>
+  <h2>Aguarde...</h2>
+</template>
