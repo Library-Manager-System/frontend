54754bf5a0534bfd90dd0cf77bb6d482611bc1eb
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2022-11-01 17:02:18 -0300

    Add protected route component

---
+++ src/components/ProtectedRoute.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { useAuth } from "@/stores/auth";
+import RedirectTo from "@/components/RedirectTo.vue";
+
+export default {
+  data: () => ({
+    authStore: useAuth(),
+  }),
+
+  components: {
+    RedirectTo,
+  },
+};
+</script>
+
+<template>
+  <slot v-if="authStore.userAuthenticated" />
+  <RedirectTo name="auth.login" v-else />
+</template>
