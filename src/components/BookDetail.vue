d615dade69c7072a677b912ecb1fb8f271cf9f60
Snikks <luccaoliari@gmail.com>
2022-11-11 12:28:13 -0300

    update resized book cover

--- src/components/BookDetail.vue
+++ src/components/BookDetail.vue
@@ -19,6 +19,8 @@ export default {
       <img
         :src="'https://covers.openlibrary.org/b/isbn/' + isbn + '-M.jpg'"
         :alt="title"
+        :width="100"
+        :height="150"
       />
       <figcaption class="title">{{ title }}</figcaption>
     </figure>
@@ -28,12 +30,13 @@ export default {
 <style scoped>
 .book {
   width: 100px;
-  height: 150px;
+  height: 140px;
   display: flex;
   align-items: center;
   justify-content: center;
   border: 1px solid var(--shadow-color);
   background-color: #fff;
+  margin-bottom: 2.7rem;
 
   cursor: pointer;
   transition: 0.2s transform;
@@ -47,5 +50,7 @@ export default {
   font-weight: bold;
   text-align: center;
   font-size: 1rem;
+
+  margin-top: 5px;
 }
 </style>
