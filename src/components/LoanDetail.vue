d45eebce9ff55ba0dd163fd553172fef385ae5fd
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2022-11-22 11:02:31 -0300

    Add loan detail component

---
+++ src/components/LoanDetail.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    title: String,
+  },
+});
+</script>
+
+<template>
+  {{ title }}
+</template>
+
+<style scoped></style>
