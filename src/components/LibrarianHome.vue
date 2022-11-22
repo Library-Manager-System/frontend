98444592297b8b1ef586305761e542baf775fbb1
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2022-11-22 11:03:25 -0300

    Add user and librarian homes

---
+++ src/components/LibrarianHome.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import ProtectedRoute from "@/components/ProtectedRoute.vue";
+import { useAuth } from "@/stores/auth";
+import { defineComponent } from "vue";
+import LoanDetail from "./LoanDetail.vue";
+
+export default defineComponent({
+  data: () => ({
+    authStore: useAuth(),
+    loans: [],
+    pendingLoans: [],
+  }),
+
+  components: {
+    ProtectedRoute,
+    LoanDetail,
+  },
+});
+</script>
+
+<template>
+  <ProtectedRoute>
+    <h2>Empréstimos pendentes</h2>
+    <ul>
+      <li>
+        <LoanDetail
+          v-for="pendingLoan of pendingLoans"
+          :key="pendingLoan"
+          title=":)"
+        />
+      </li>
+    </ul>
+  </ProtectedRoute>
+</template>
