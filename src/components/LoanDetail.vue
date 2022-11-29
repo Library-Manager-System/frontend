5b784a08337871e31b79f2ff6e475660e3eef87f
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2022-11-29 11:28:38 -0300

    Update librarian view and loan detail component

--- src/components/LoanDetail.vue
+++ src/components/LoanDetail.vue
@@ -40,26 +40,56 @@ export default defineComponent({
 
 <template>
   <div class="loan-detail">
-    <span>
-      <strong>{{ loan.title }} </strong>
-      <span> - Solicitado por </span>
-      <strong>{{ loan.email }}</strong>
-      <span> em </span>
-      <strong>{{ new Date(loan.dt_loan).toLocaleDateString() }}</strong>
-      <span> - Status: </span>
-      <strong>{{
-        loan.approved_loan
-          ? loan.returned_loan
-            ? "Devolvido"
-            : "Aprovado"
-          : "Aguardando aprovação"
-      }}</strong>
-    </span>
-    <div>
-      <button @click="authorizeLoan" v-if="!loan.approved_loan">Aprovar</button>
-      <button @click="returnLoan" v-else-if="!loan.returned_loan">
-        Devolver
-      </button>
+    <div class="book-container">
+      <img
+        :src="`https://covers.openlibrary.org/b/isbn/${loan.isbn}-M.jpg`"
+        :alt="loan.title"
+        loading="lazy"
+        class="book-cover"
+      />
+      <div>
+        <strong>{{ loan.title }}</strong>
+        <br /><br />
+        <span>
+          <strong>ISBN: </strong>
+          {{ loan.isbn }}
+        </span>
+        <br />
+        <span>
+          <strong>Solicitado por: </strong>
+          <a :href="`mailto:${loan.email}`">
+            {{ loan.email }}
+          </a>
+        </span>
+        <br />
+        <span>
+          <strong>Solicitado em: </strong>
+          {{ new Date(loan.dt_loan).toLocaleDateString() }}</span
+        >
+        <br />
+        <span>
+          <strong>Status: </strong>
+          {{
+            loan.approved_loan
+              ? loan.returned_loan
+                ? "Devolvido"
+                : "Aprovado"
+              : "Aguardando aprovação"
+          }}
+        </span>
+        <br /><br />
+        <button @click="authorizeLoan" v-if="!loan.approved_loan">
+          Aprovar
+        </button>
+        <button
+          @click="returnLoan"
+          class="button-error"
+          v-else-if="!loan.returned_loan"
+        >
+          Devolver
+        </button>
+        <button class="button-disabled" v-else>Devolvido</button>
+      </div>
     </div>
   </div>
 </template>
@@ -69,5 +99,69 @@ export default defineComponent({
   display: flex;
   align-items: center;
   justify-content: space-between;
+  border: 1px solid #000;
+  padding: 1rem;
+  border: 1px solid var(--shadow-color);
+  background-color: #fff;
+  box-shadow: 2px 2px 0px var(--shadow-color);
+  width: 400px;
+}
+
+.book-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+}
+
+.book-cover {
+  width: 120px;
+  height: 180px;
+  border: 1px solid var(--shadow-color);
+}
+
+button {
+  padding: 10px 15px;
+  border: 1px solid var(--shadow-color);
+  box-shadow: 2px 2px 0px var(--shadow-color);
+  outline: none;
+  background-color: var(--sucess-color);
+  font-weight: bold;
+  transition: ease-in-out 0.1s;
+}
+
+button:hover {
+  box-shadow: 4px 4px 0px var(--shadow-color);
+  transform: translate(-2px, -2px);
+}
+
+button:active {
+  box-shadow: 0px 0px 0px var(--shadow-color);
+  transform: translate(1px, 1px);
+}
+
+.button-error {
+  background-color: var(--error-color);
+}
+
+.button-disabled,
+.button-disabled:hover,
+.button-disabled:active {
+  opacity: 0.8;
+  box-shadow: none;
+  transform: none;
+}
+
+.button-disabled {
+  cursor: default;
+}
+
+@media screen and (max-width: 768px) {
+  .loan-detail {
+    width: 100%;
+    border: none;
+    box-shadow: none;
+    padding: 0;
+  }
 }
 </style>
--- src/views/librarian/LibrarianView.vue
+++ src/views/librarian/LibrarianView.vue
@@ -44,30 +44,31 @@ export default defineComponent({
   <ProtectedRoute>
     <PermissionLevel :permission-level="2">
       <h2>Empréstimos</h2>
-      <ul v-if="loans.length > 0" class="loan-list">
-        <li v-for="loan of loans" :key="loan.id_loan" class="loan-item">
-          <LoanDetail :loan="loan" @refresh="fetchLoans" />
-        </li>
-      </ul>
+      <div v-if="loans.length > 0" class="loan-container">
+        <LoanDetail
+          v-for="loan of loans"
+          :key="loan.id_loan"
+          :loan="loan"
+          @refresh="fetchLoans"
+        />
+      </div>
     </PermissionLevel>
   </ProtectedRoute>
 </template>
 
 <style scoped>
-.loan-list {
-  padding-left: 0;
-  list-style-type: none;
-  width: 80%;
-  display: flex;
-  flex-direction: column;
-  gap: 0.7rem;
+.loan-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+  padding: 1rem;
 }
 
-.loan-item {
-  border: 1px solid #000;
-  padding: 1rem;
-  border: 1px solid var(--shadow-color);
-  background-color: #fff;
-  box-shadow: 2px 2px 0px var(--shadow-color);
+@media screen and (max-width: 768px) {
+  .loan-container {
+    grid-template-columns: 1fr;
+    width: 100%;
+    margin: auto;
+  }
 }
 </style>
